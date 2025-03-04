1f3452a55c519b65ba718850e58216a3e84a8b2c
DOYEON KIM <80318425+doyeonk429@users.noreply.github.com>
2025-03-04 10:47:00 +0900

    [level 2] Title: 기능개발, Time: 0.12 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.swift
+++ 프로그래머스/2/42586. 기능개발/기능개발.swift
@@ -1,45 +1,24 @@
 import Foundation
-// 5 10 1 1 20 1
-// 0번은 무조건 걍 나감
-// 10 나갈때 뒤에 1 1도 나감
-
-// 소요 일수 계산법..
-// 100 - progresses[i] = restPerscent
-// restpercent % speeds[i] == 0 -> restpercent / speeds[i]
-// restpercent / speeds[i] + 1
 
 func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
-    var days = Array(repeating: 0, count: progresses.count)
-    
-    for (i, p) in progresses.enumerated() {
-        let rest = 100 - p
-        let sp = speeds[i]
-        days[i] = rest / sp
-        if rest % sp != 0 {
-            days[i] += 1
-        }
+    let days: [Int] = progresses.indices.map { 
+        let remain = 100 - progresses[$0]
+        return (remain + speeds[$0] - 1) / speeds[$0] 
     }
-    
-    // days에서 firstDay 설정
-    // firstDay보다 작은 값들은 count++
-    // firstDay보다 큰 값나오면 지금까지의 count를 Result에 추가 후 firstDay 업데이트
-    // 마지막 남은 개수도 result 배열에 추가
-    
     var result = [Int]()
-    var firstDay = days[0]
-    var count = 1
-    
-    for i in 1..<days.count {
-        if days[i] <= firstDay {
-            // 같이 배포 가능
-            count += 1
+    var prevMax = days[0]
+    var count = 0
+
+    for day in days {
+        if day > prevMax {
+            result.append(count)
+            count = 1
+            prevMax = day
         } else {
-            result.append(count) // 지금까지의 카운트 추가
-            firstDay = days[i] // firstday 업데이트
-            count = 1 // 카운트도 재시작
+            count += 1
         }
     }
-    result.append(count) // 마지막 배포 개수 추가
     
+    result.append(count) // 마지막 배포 추가
     return result
 }
