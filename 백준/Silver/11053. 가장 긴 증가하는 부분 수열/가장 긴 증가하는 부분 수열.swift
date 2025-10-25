2a7356f061bcfde50272b21b44506fc50cddb273
DOYEON KIM <80318425+doyeonk429@users.noreply.github.com>
2025-10-25 23:57:08 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 12 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.swift
@@ -0,0 +1,13 @@
+let n = Int(readLine()!)!
+let array = readLine()!.split(separator: " ").map { Int($0)! }
+var cache = [Int](repeating: 1, count: n)
+
+for i in 1..<n {
+    for j in 0..<i {
+        if array[i] > array[j] {
+            cache[i] = max(cache[i], cache[j] + 1)
+        }
+    }
+}
+
+print(cache.max()!)
